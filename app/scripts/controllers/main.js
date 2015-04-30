0f6379a5a58ec5d4a55d09e20ba3b9b5d841c865
lucatrincauk <lucatrinca.uk@gmail.com>
2015-04-30 16:11:16 +0100

    separate filters from Dom

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,20 +8,22 @@
  * Controller of the bonusApp
  */
 angular.module('bonusApp')
-  .controller('MainCtrl', function ($scope, $http) {
-  $http.get('data/bonus.json').
-    success(function(data) {
-      $scope.bonus = data;
-      console.log('success');
-    }).
-    error(function(status) {
-      console.log(status);
-    });
-    $scope.selected = {};
-    $scope.filterResult = function(target, level) {
-    return function(result) {
-        return (level === undefined || level.length === 0 || result.level === level);
-    };
-};
-});
+	.controller('MainCtrl', function($scope, $http) {
 
+		$http.get('data/bonus.json')
+			.success(function(data) {
+				$scope.bonus = data;
+						$scope.bonus.selected = {};
+
+			})
+			.error(function(status) {
+				console.log(status);
+			});
+
+		$scope.filterResult = function(rating, level) {
+			return function(result) {
+				result.money = $scope.bonus.selected.salary * result.score / 100;
+				return (level === undefined || level.length === 0 || result.level === level) && (rating === undefined || rating.length === 0 || result.rating === rating);
+			};
+		};
+	});
