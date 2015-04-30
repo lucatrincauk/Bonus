067da6edd32e44e2d7c6071b4eea2a1bc156daca
lucatrincauk <lucatrinca.uk@gmail.com>
2015-04-30 13:33:05 +0100

    removed one step

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,11 +22,9 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/materialize/bin/materialize.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
